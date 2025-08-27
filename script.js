2b72135d70c31a906cf9432349eaa94cc964543a
MaksudulAlam1 <maksudulalam123456@gmail.com>
2025-08-28 00:44:50 +0600

    my 2nd commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+document.getElementById("call-btn").addEventListener("click",function(){
+    alert("National Number clicked, 999")
+})
+document.getElementById("police-btn").addEventListener("click",function(){
+    alert("Police Helpline number, 999")
+})
+document.getElementById("fire-call").addEventListener("click",function(){
+    alert("Fire service Number, 999")
+})
+document.getElementById("ambulance-call").addEventListener("click",function(){
+    alert("Ambulace Number, 1994-999999")
+})
+document.getElementById("women-call").addEventListener("click",function(){
+    alert("Women Child Helpline, 109")
+})
+document.getElementById("anti-call").addEventListener("click",function(){
+    alert("Anti-corruption Helpline, 106")
+})
+document.getElementById("electricity-call").addEventListener("click",function(){
+    alert("Electricity Helpline, 16216")
+})
+document.getElementById("brac-call").addEventListener("click",function(){
+    alert("Brac helpline, 16445")
+})
+document.getElementById("bd-railway-call").addEventListener("click",function(){
+    alert("Bangladesh railway helpline, 163")
+})
